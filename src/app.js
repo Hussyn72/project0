2059b6aacec6202137777322985541cc0513e584
[Mohd <[mohammadhussain5601@gmail.com]>
2026-02-11 20:32:23 +0530

    createdjwtAuth

--- src/app.js
+++ src/app.js
@@ -8,11 +8,14 @@ const connectDB = require("./config.js/database");
 const userModel = require("./models/userModel");
 const { validateSignUpData } = require("./utils/validation");
 const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true })); // for form data
 
 //SignUp API
@@ -20,8 +23,8 @@ app.post("/signup", async (req, res) => {
   try {
     const { FirstName, LastName, Age, Password, Gender, EmailId } = req.body;
 
-    const userExist = await userModel.findOne({EmailId : EmailId});
-    if(userExist){
+    const userExist = await userModel.findOne({ EmailId: EmailId });
+    if (userExist) {
       return res.status(409).send("User Already Exists");
     }
 
@@ -43,8 +46,8 @@ app.post("/signup", async (req, res) => {
     await user.save();
     res.status(200).json({ message: "User Created Successfully", user });
   } catch (error) {
-    if(error.code === 11000){
-      return res.status(409).json({message:"user already exists"});
+    if (error.code === 11000) {
+      return res.status(409).json({ message: "user already exists" });
     }
     res.status(400).json({ error: error.message });
   }
@@ -54,20 +57,49 @@ app.post("/signup", async (req, res) => {
 app.post("/login", async (req, res) => {
   try {
     const { EmailId, Password } = req.body;
-    const userExists =  await userModel.findOne({ EmailId: EmailId });
+    const userExists = await userModel.findOne({ EmailId: EmailId });
     if (!userExists) {
       console.log("User Email is not present in the Database");
       res.status(404).send("User Not Found");
     } else {
-      const isPasswordCorrect = await bcrypt.compare(Password, userExists.Password);
-      if(isPasswordCorrect){
+      const isPasswordCorrect = await bcrypt.compare(
+        Password,
+        userExists.Password
+      );
+      if (isPasswordCorrect) {
+        //Create JWT Token
+        const token = await jwt.sign(
+          { _id: userExists._id },
+          "SadluTuhai@chaiwala"
+        );
+        console.log(token);
+
+        //Add the token to cookie and send the response back to user
+        res.cookie("JWTtoken", token);
         res.status(200).send("Logged In Successfully");
-      }else{
+      } else {
         res.status(400).send("Password is not Correct");
       }
     }
   } catch (error) {
     res.status(400).send("Login Failed !" + error.message);
+  }
+});
+
+//Profile API
+app.get("/profile", async (req, res) => {
+  try {
+    const cookies = req.cookies;
+    const { JWTtoken } = cookies;
+    if (!JWTtoken) {
+      throw new Error("UnAuthorized Request");
+    }
+    const isTokenValid = await jwt.verify(JWTtoken, "SadluTuhai@chaiwala");
+    const { _id } = isTokenValid;
+    const gotUser = await userModel.findOne({ _id: _id });
+    res.status(200).send(gotUser);
+  } catch (error) {
+    res.status(401).send(error.message);
   }
 });
 
